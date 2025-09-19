aaf7370b4e995eb1e386991819619d2d3a83c610
rawahazkhan <rawahazkhan05@gmail.com>
2025-09-19 13:07:48 +0500

    added the loop and finalized the game

--- Word Guessing Game/main.py
+++ Word Guessing Game/main.py
@@ -10,5 +10,26 @@ word = random.choice(word_bank)
 guess_word = ['_'] * len(word)
 attempts = 10
 
+while attempts > 0:
+    print("\nCurrent word: " + " ".join(guess_word))
+    guess = input("Guess the letter: ").lower()
+    
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guess_word[i] = guess
+                print("Wow! You guessed right!")
+    else:
+        attempts -= 1
+        print("Oop! Wrong Guess, Attempts left: " + str(attempts))
+
+    if "_" not in guess_word:
+        print("\nHurray! You guess the word: " + word)
+        break
+
+if attempts == 0 and "_" in guess_word:
+    print("\nOop! You\'ve run out of attempts. The word was: " + word)
+     
+
 
 
